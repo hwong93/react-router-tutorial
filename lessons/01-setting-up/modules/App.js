ddef418c1ce663bade58ba94e4b489e92bbcabd8
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-06-21 17:19:15 -0400

    react routes

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import Home from './Home'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+      <div>
+        <h1> React Router Tutorial </h1>
+        <ul role="nav">
+          <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+
+          <li><NavLink to="/about" >About</NavLink></li>
+          <li><NavLink to="/repos">Repos</NavLink></li>
+        </ul>
+
+        {this.props.children}
+      </div>
+
+    )
   }
 })
